beed9e111087464e420d370837164fa9840303ca
Amanda Mesquita <amandamesquita73638@gmail.com>
2024-09-02 20:47:11 -0300

    first commit

---
+++ NASA/src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NasaPhoto from "./components/nasaPhoto";
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>OLHA QUE LINDO!</h1>
+      <NasaPhoto />
+    </div>
+  );
+};
+
+export default App;
